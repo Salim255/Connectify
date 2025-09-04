9bf820dac4f21f1aaa08d33596d845e0d7765ff8
Salim <salim.hassan.welleat@gmail.com>
2025-09-05 01:40:37 +0200

    added: matches component

---
+++ client/src/app/features/matches/matches-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from "@angular/router";
+import { MatchesComponent } from "./matches.component";
+import { NgModule } from "@angular/core";
+
+const routes:Routes = [
+  {
+    path: '',
+    component: MatchesComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MatchesRoutingModule{}
