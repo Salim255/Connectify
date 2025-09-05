67c979f57b4096a0e92fe73f1f9a82181e59052b
Salim <salim.hassan.welleat@gmail.com>
2025-09-05 06:16:41 +0200

    built: database connection

--- servers/server/src/database/services/database.service.ts
+++ servers/server/src/database/services/database.service.ts
@@ -1,32 +1,34 @@
-import { Logger } from '@nestjs/common';
+import { Logger, OnModuleInit } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
-export class DatabaseService {
+export class DatabaseService implements OnModuleInit {
   private readonly logger = new Logger('MONGODB');
+  constructor(@InjectConnection() private readonly connection: Connection) {}
 
-  logConnectionEvents(connection: Connection) {
+  onModuleInit() {
     // Triggered when the connection is successfully established.
-    connection.on('connected', () => {
+    this.connection.on('connected', () => {
       this.logger.log('✅ MongoDB connected successfully...');
     });
 
     // Fires when the connection is fully opened and ready for operations
-    connection.on('open', () => {
+    this.connection.on('open', () => {
       this.logger.log('✅ MongoDB is ready for operation...');
     });
 
     // Invoked when the connection is re-established after being disconnected.
-    connection.on('reconnected', () => {
+    this.connection.on('reconnected', () => {
       this.logger.log('✅ reconnected');
     });
 
     // Occurs when the connection is in the process of closing.
-    connection.on('disconnecting', () => {
+    this.connection.on('disconnecting', () => {
       this.logger.log('💥 MongoDB connection is disconnecting');
     });
 
     // Called when the connection is lost.
-    connection.on('disconnected', () => {
+    this.connection.on('disconnected', () => {
       this.logger.log('💥 MongoDB disconnected');
     });
   }
