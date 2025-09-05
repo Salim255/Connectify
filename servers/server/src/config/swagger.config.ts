8a9935564448c65e2ae96cecd9508f2fefaeb806
Salim <doshka23@gmail.com>
2025-09-05 05:00:08 +0200

    improved: swagger config

--- servers/server/src/config/swagger.config.ts
+++ servers/server/src/config/swagger.config.ts
@@ -2,16 +2,17 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
+  const globalPrefix = (process.env.API_PREFIX ?? 'api') + '/v1';
+  app.setGlobalPrefix(globalPrefix); // All routes will be prefixed with /api/v1
+
   const config = new DocumentBuilder()
     .setTitle('Connectify API')
     .setDescription('API for modern dating application')
     .setVersion('1.0')
     .addServer('/api/v1') // 👈 Add this line to fix the prefix issue
-    .addCookieAuth('optional-session-id')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  app.setGlobalPrefix((process.env.API_PREFIX ?? 'api') + '/v1'); // Set a global prefix for all routes
 
   // Setup Swagger UI at /api
   SwaggerModule.setup('api', app, document);
