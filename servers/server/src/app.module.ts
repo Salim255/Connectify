b27dad4cdd4206c50c59f203853988b475bb5004
Salim <doshka23@gmail.com>
2025-09-05 05:15:14 +0200

    added: config module

--- servers/server/src/app.module.ts
+++ servers/server/src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatsModule } from './modules/chats/chats.module';
 import { MatchesModule } from './modules/matches/matches.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MatchesModule, AuthModule, ChatsModule],
+  imports: [
+    MatchesModule,
+    AuthModule,
+    ChatsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
