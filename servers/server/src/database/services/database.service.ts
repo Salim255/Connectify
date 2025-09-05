739aa9f331fd46c66438d6bedeb26c345cc4851e
Salim <salim.hassan.welleat@gmail.com>
2025-09-05 05:46:40 +0200

    built: database module

--- servers/server/src/database/services/database.service.ts
+++ servers/server/src/database/services/database.service.ts
@@ -1,3 +1,33 @@
+import { Logger } from '@nestjs/common';
+import { Connection } from 'mongoose';
+
 export class DatabaseService {
-  constructor() {}
+  private readonly logger = new Logger('MONGODB');
+
+  logConnectionEvents(connection: Connection) {
+    // Triggered when the connection is successfully established.
+    connection.on('connected', () => {
+      this.logger.log('✅ MongoDB connected successfully...');
+    });
+
+    // Fires when the connection is fully opened and ready for operations
+    connection.on('open', () => {
+      this.logger.log('✅ MongoDB is ready for operation...');
+    });
+
+    // Invoked when the connection is re-established after being disconnected.
+    connection.on('reconnected', () => {
+      this.logger.log('✅ reconnected');
+    });
+
+    // Occurs when the connection is in the process of closing.
+    connection.on('disconnecting', () => {
+      this.logger.log('💥 MongoDB connection is disconnecting');
+    });
+
+    // Called when the connection is lost.
+    connection.on('disconnected', () => {
+      this.logger.log('💥 MongoDB disconnected');
+    });
+  }
 }
