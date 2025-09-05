8e2202898d50d85f74c5c981128c4e97d048950b
Salim <salim.hassan.welleat@gmail.com>
2025-09-05 04:37:35 +0200

    added: server chats module structure

--- servers/server/src/app.module.ts
+++ servers/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { ChatsModule } from './modules/chats/chats.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ChatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
