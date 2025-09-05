3915d23b7caf346c28f51cf13a462c98632d9dcd
Salim <salim.hassan.welleat@gmail.com>
2025-09-05 04:43:41 +0200

    added: server matches struture

--- servers/server/src/app.module.ts
+++ servers/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatsModule } from './modules/chats/chats.module';
+import { MatchesModule } from './modules/matches/matches.module';
 
 @Module({
-  imports: [AuthModule, ChatsModule],
+  imports: [MatchesModule, AuthModule, ChatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
