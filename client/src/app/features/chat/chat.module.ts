9ba796fafb07a6a4f0bd4ae3c3fe8aa2d4352425
Salim <salim.hassan.welleat@gmail.com>
2025-09-10 15:44:16 +0200

    built: message form component

--- client/src/app/features/chat/chat.module.ts
+++ client/src/app/features/chat/chat.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 import { ChatRoutingModule } from "./chat-routing.module";
 import { ChatComponent } from "./chat.component";
+import { ChatFomComponent } from "./components/chat-form/chat-form.component";
 
 @NgModule({
   imports: [
@@ -12,6 +13,6 @@ import { ChatComponent } from "./chat.component";
     FormsModule,
     ChatRoutingModule
   ],
-  declarations: [ChatComponent]
+  declarations: [ChatFomComponent, ChatComponent]
 })
 export class ChatModule {}
