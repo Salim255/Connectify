111bee3f41baf119c17fc482da75509b4a88f777
Salim <salim.hassan.welleat@gmail.com>
2025-09-10 15:15:42 +0200

    added: chat form structure

---
+++ client/src/app/features/chat/components/chat-form/chat-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-chat-form',
+  templateUrl: './chat-form.component.html',
+  styleUrls: ['./chat-form.component.scss'],
+  standalone: false,
+})
+
+
+export class ChatFomComponent {}
