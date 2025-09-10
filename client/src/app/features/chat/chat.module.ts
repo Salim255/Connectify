531fc38c7d26afb92b6cc649c26ae438d831fcf9
Salim <doshka23@gmail.com>
2025-09-10 17:16:22 +0200

    added: chat header

--- client/src/app/features/chat/chat.module.ts
+++ client/src/app/features/chat/chat.module.ts
@@ -7,6 +7,7 @@ import { ChatComponent } from "./chat.component";
 import { ChatFomComponent } from "./components/chat-form/chat-form.component";
 import { MessageBubbleComponent } from "./components/message-bubble/message-bubble.component";
 import { MessagesComponent } from "./components/messages/messages.component";
+import { ChatHeaderComponent } from "./components/chat-header/chat-header";
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { MessagesComponent } from "./components/messages/messages.component";
     ChatRoutingModule
   ],
   declarations: [
+    ChatHeaderComponent,
     MessagesComponent,
     MessageBubbleComponent,
     ChatFomComponent,
