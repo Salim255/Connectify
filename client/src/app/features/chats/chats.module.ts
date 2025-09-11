f4e0ac49d0e9ffdd155141995c0f912af8927242
Salim <salim.hassan.welleat@gmail.com>
2025-09-12 00:24:33 +0200

    added: header to chats page

--- client/src/app/features/chats/chats.module.ts
+++ client/src/app/features/chats/chats.module.ts
@@ -4,12 +4,14 @@ import { ChatsRoutingModule } from "./chats-routing.module";
 import { IonicModule } from "@ionic/angular";
 import { CommonModule } from "@angular/common";
 import { ChatItemComponent } from "./components/chat-item/chat-item.component";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    ChatsRoutingModule
+    ChatsRoutingModule,
+    SharedModule
   ],
   declarations: [ChatItemComponent ,ChatsComponent]
 })
