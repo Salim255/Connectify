2319c8914d9bd4f6976dae17a0b8e4f002623734
Salim <doshka23@gmail.com>
2025-09-11 22:11:14 +0200

    integrated: chats

--- client/src/app/features/matches/services/matches.service.ts
+++ client/src/app/features/matches/services/matches.service.ts
@@ -5,45 +5,47 @@ import { ProfileService } from "../../profile/services/profile.service";
 
 @Injectable({providedIn: 'root'})
 export class MatchesService {
-  profiles: Profile [] = [];
+  profiles: Profile [];
+  MATCHES_PLACEHOLDER: Match[];
   constructor(private profileService: ProfileService){
     this.profiles = this.profileService.PROFILES_PLACEHOLDER;
+    this.MATCHES_PLACEHOLDER = [
+      {
+        id: '1',
+        profile: this.profiles[0],
+        matchedAt: new Date(),                     // timestamp of the match
+        isMutual: true,                 // true if both liked
+        seen: true,
+      },
+      {
+        id: '2',
+        profile: this.profiles[1],
+        matchedAt: new Date(),                     // timestamp of the match
+        isMutual: true,                 // true if both liked
+        seen: true,
+      },
+      {
+        id: '3',
+        profile: this.profiles[2],
+        matchedAt: new Date(),                     // timestamp of the match
+        isMutual: true,                 // true if both liked
+        seen: true,
+      },
+      {
+        id: '4',
+        profile: this.profiles[0],
+        matchedAt: new Date(),                     // timestamp of the match
+        isMutual: true,                 // true if both liked
+        seen: true,
+      },
+      {
+        id: '5',
+        profile: this.profiles[0],
+        matchedAt: new Date(),                     // timestamp of the match
+        isMutual: true,                 // true if both liked
+        seen: true,
+      },
+    ];
   }
-  MATCHES_PLACEHOLDER: Match[] = [
-    {
-      id: '1',
-      profile: this.profiles[0],
-      matchedAt: new Date(),                     // timestamp of the match
-      isMutual: true,                 // true if both liked
-      seen: true,
-    },
-    {
-      id: '2',
-      profile: this.profiles[1],
-      matchedAt: new Date(),                     // timestamp of the match
-      isMutual: true,                 // true if both liked
-      seen: true,
-    },
-    {
-      id: '3',
-      profile: this.profiles[2],
-      matchedAt: new Date(),                     // timestamp of the match
-      isMutual: true,                 // true if both liked
-      seen: true,
-    },
-    {
-      id: '4',
-      profile: this.profiles[0],
-      matchedAt: new Date(),                     // timestamp of the match
-      isMutual: true,                 // true if both liked
-      seen: true,
-    },
-    {
-      id: '5',
-      profile: this.profiles[0],
-      matchedAt: new Date(),                     // timestamp of the match
-      isMutual: true,                 // true if both liked
-      seen: true,
-    },
-  ];
+
 }
