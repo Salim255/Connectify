6d63897f67634829f3d19701613d08f5f3a5ddfd
Salim <doshka23@gmail.com>
2025-09-12 02:08:42 +0200

    added: custom grid

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./components/header/header.component";
 import { IonicModule } from "@ionic/angular";
+import { CustomGridComponent } from "./kits/custom-grid/custom-grid.component";
 
 @NgModule({
   imports: [ IonicModule],
-  declarations: [ HeaderComponent ],
-  exports: [ HeaderComponent ]
+  declarations: [CustomGridComponent, HeaderComponent ],
+  exports: [CustomGridComponent,  HeaderComponent ]
 })
 export class SharedModule {
 
