7fe15bb57c5ab319ca4c165bf7e7ee64a03582d9
Salim <doshka23@gmail.com>
2025-09-12 16:31:17 +0200

    built: account profile

---
+++ client/src/app/features/account/components/account-profile/account-profile-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { AccountProfileComponent } from "./account-profile.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AccountProfileComponent,
+    children: [
+        {
+        path: 'view',
+        loadChildren: () => import('../view-profile/view-profile.module').then(m => m.ViewProfileModule)
+      },
+      {
+        path: 'edit',
+        loadChildren: () => import('../edit-profile/edit-profile.module').then(m => m.EditProfileModule)
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class AccountProfileRoutingModule {}
