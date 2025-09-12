c736be7debe7250e2a227adf69e17a917e39dc6f
Salim <salim.hassan.welleat@gmail.com>
2025-09-12 19:41:10 +0200

    built: account profile toggle header

--- client/src/app/features/account/components/account-profile/account-profile.component.ts
+++ client/src/app/features/account/components/account-profile/account-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { AccountHeaderService } from "../../services/account-header.service";
 
 @Component({
@@ -9,8 +9,17 @@ import { AccountHeaderService } from "../../services/account-header.service";
 })
 
 export class AccountProfileComponent {
-  constructor(private accountHeaderService: AccountHeaderService){}
 
+  headerTitle = signal<string>('view profile');
+
+  constructor(private accountHeaderService: AccountHeaderService){}
+  ngOnInit(): void {
+    this.accountHeaderService.setHeaderHide(true);
+  }
+  onToggle(input: 'view profile' | 'edit profile'){
+    if (this.headerTitle() === input ) return;
+    this.headerTitle.set(input);
+  }
   onBack(): void{
     this.accountHeaderService.setHeaderHide(false);
   }
