250b770c20ac56932ab4dc8ff10924d8e2bc4a51
Salim <doshka23@gmail.com>
2025-09-12 16:19:13 +0200

    added: acount profile component

--- client/src/app/features/account/components/account-dashboard/account-dashboard.module.ts
+++ client/src/app/features/account/components/account-dashboard/account-dashboard.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from "@angular/common";
-import { Component, NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
 import { AccountDashboardComponent } from "./account-dashboard.component";
 import { AccountDashboardRoutingModule } from "./account-dashboard-routing.module";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
-  imports: [AccountDashboardRoutingModule,CommonModule, IonicModule],
+  imports: [
+    AccountDashboardRoutingModule,
+    CommonModule,
+    IonicModule,
+    SharedModule
+  ],
   declarations: [AccountDashboardComponent]
 })
 
