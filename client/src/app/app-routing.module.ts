b90075ed610c1c5b25af89402c6f206193c81edb
Salim <salim.hassan.welleat@gmail.com>
2025-09-14 22:07:51 +0200

    integrated auth in app routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsModule),
+    canLoad: [],
   }
 ];
 
