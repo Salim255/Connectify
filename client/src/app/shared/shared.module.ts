7557abc04721b95200f2a051663a99724504c4f7
Salim <doshka23@gmail.com>
2025-09-14 17:54:08 +0200

    added: location picker component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { HeaderComponent } from "./components/header/header.component";
 import { IonicModule } from "@ionic/angular";
 import { CustomGridComponent } from "./kits/custom-grid/custom-grid.component";
+import { LocationPickerComponent } from "./components/location-picker/location-picker";
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [ IonicModule],
-  declarations: [CustomGridComponent, HeaderComponent ],
-  exports: [CustomGridComponent,  HeaderComponent ]
+  declarations: [LocationPickerComponent, CustomGridComponent, HeaderComponent ],
+  exports: [LocationPickerComponent, CustomGridComponent,  HeaderComponent ]
 })
 export class SharedModule {
 
