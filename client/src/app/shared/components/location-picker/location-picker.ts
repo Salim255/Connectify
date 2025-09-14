77e1f9d7b8da1bb4b4a0347440f5b74d456865da
Salim <salim.hassan.welleat@gmail.com>
2025-09-14 17:54:08 +0200

    added: location picker component

---
+++ client/src/app/shared/components/location-picker/location-picker.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-location-picker',
+  templateUrl: './location-picker.html',
+  standalone: false
+})
+
+export class LocationPickerComponent {}
