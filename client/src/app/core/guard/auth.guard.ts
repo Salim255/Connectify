506833755ca8b9062b9ae95950e8200b1b8cffe0
Salim <doshka23@gmail.com>
2025-09-14 22:26:36 +0200

    added: auth service

--- client/src/app/core/guard/auth.guard.ts
+++ client/src/app/core/guard/auth.guard.ts
@@ -1,1 +1,16 @@
-export class AuthGuard {}
+import { Injectable } from "@angular/core";
+import { CanMatch, Route, UrlSegment } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "src/app/features/auth/services/auth.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthGuard implements CanMatch {
+  constructor(private authService:  AuthService){}
+  canMatch(
+    route: Route, segments: UrlSegment[],
+  ): Observable<boolean> | Promise<boolean> | boolean {
+   return  this.authService.isAuthenticated;
+  }
+}
---
+++ client/src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: "root"})
+
+export class AuthService {
+  private _isAuthenticated = false;
+
+  get isAuthenticated() {
+    return this._isAuthenticated;
+  }
+
+  login() {
+    this._isAuthenticated = true;
+  }
+
+  logout() {
+    this._isAuthenticated = false;
+  }
+}
