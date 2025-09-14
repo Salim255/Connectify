515fc5ecaa69c9a1f5f5387fd586fa05010ab446
Salim <doshka23@gmail.com>
2025-09-14 16:51:05 +0200

    added: google map service

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from "@angular/common";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   imports: [
-
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers:[
+    provideHttpClient(withInterceptorsFromDi())
   ]
 })
 export class CoreModule {}
