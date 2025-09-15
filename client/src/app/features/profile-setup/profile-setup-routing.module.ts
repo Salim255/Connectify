7047988d7fc36d359253a7e60e9a2173bc79513a
Salim <doshka23@gmail.com>
2025-09-15 19:05:35 +0200

    built: profile-setup routing module

---
+++ client/src/app/features/profile-setup/profile-setup-routing.module.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from "@angular/router";
+import { ProfileSetupComponent } from "./profile-setup.component";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  { path: '', component: ProfileSetupComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileSetupRoutingModule {}
---
+++ client/src/app/features/profile-setup/profile-setup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+
+@Component({
+  selector: 'app-profile-setup',
+  templateUrl: './profile-setup.component.html',
+  styleUrls: ['./profile-setup.component.scss'],
+  imports: [IonicModule]
+})
+export class ProfileSetupComponent {}
