245f68ccd5461de6fe6469ce57965a5103e19ef4
Salim <doshka23@gmail.com>
2025-09-16 01:49:11 +0200

    connected: to local posgresql databse

---
+++ servers/server/src/database/providers/database.provider.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const databaseProviders = {
+  provide: 'DATA_SOURCE',
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
+    //const logger = new Logger('DatabaseProvider');
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_NAME'),
+      synchronize: true, // ❌ disable in production
+      logging: true,
+    });
+
+    return dataSource.initialize();
+  },
+};
