76312df6c3fdd7815f19fd0618d616a3642c4218
Salim <salim.hassan.welleat@gmail.com>
2025-09-16 00:10:26 +0200

    built: profile setup form

--- client/src/app/features/profile-setup/profile-setup.module.ts
+++ client/src/app/features/profile-setup/profile-setup.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from "@angular/core";
 import { ProfileSetupComponent } from "./profile-setup.component";
 import { ProfileSetupRoutingModule } from "./profile-setup-routing.module";
 import { IonicModule } from "@ionic/angular";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProfileSetupFormComponent } from "./components/profile-setup-form/profile-setup-form.component";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
   declarations: [ProfileSetupFormComponent, ProfileSetupComponent],
   imports: [
+    SharedModule,
     IonicModule,
     FormsModule,
     ProfileSetupRoutingModule,
-  ],
+    ReactiveFormsModule,
+    SharedModule
+],
 })
 export class ProfileSetupModule {}
