527119e300c4650e6f826ef6d3933cc55a3186f7
Salim <doshka23@gmail.com>
2025-09-16 01:24:13 +0200

    removed: mogodb daabse dynamic povider

--- servers/server/src/database/database.module.ts
+++ servers/server/src/database/database.module.ts
@@ -4,46 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
 @Module({
-  imports: [
-    // MongooseModule.forRootAsync allows us to configure the MongoDB connection asynchronous
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const logger = new Logger('MONGODB');
-        const uri = config.get<string>('REMOTE_DATABASE_URL');
-        return {
-          uri,
-          dbName: 'connectify',
-          onConnectionCreate: (connection: Connection) => {
-            // Triggered when the connection is successfully established.
-            connection.on('connected', () => {
-              logger.log('✅ MongoDB connected successfully...');
-            });
-
-            // Fires when the connection is fully opened and ready for operations
-            connection.on('open', () => {
-              logger.log('✅ MongoDB is ready for operation...');
-            });
-
-            // Invoked when the connection is re-established after being disconnected.
-            connection.on('reconnected', () => {
-              logger.log('✅ reconnected');
-            });
-
-            // Occurs when the connection is in the process of closing.
-            connection.on('disconnecting', () => {
-              logger.log('💥 MongoDB connection is disconnecting');
-            });
-
-            // Called when the connection is lost.
-            connection.on('disconnected', () => {
-              logger.log('💥 MongoDB disconnected');
-            });
-            return connection;
-          },
-        };
-      },
-    }),
-  ],
+  imports: [],
 })
 export class DatabaseModule {}
---
+++ servers/server/src/database/providers/databse-provider.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'connectify',
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
