5a8a3c25cc64736da21200b45ba172280e7e6bfe
Salim <doshka23@gmail.com>
2025-09-15 17:00:32 +0200

    added: autologout logic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from './features/auth/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  private previousAuthState: boolean = false;
+  private authenticatedUserSubscription!: Subscription;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService) {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.subscribeToUserIsAuthenticated()
+  }
+
+  subscribeToUserIsAuthenticated(){
+    this.authenticatedUserSubscription = this.authService.userIsAuthenticated.subscribe((isAuthenticated) => {
+      if (!isAuthenticated && this.previousAuthState !== isAuthenticated){
+        this.router.navigateByUrl('/auth');
+      }
+      this.previousAuthState = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authenticatedUserSubscription?.unsubscribe();
+  }
 }
