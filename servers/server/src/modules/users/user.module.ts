d8c9282699b5a527b5e135fb7caeac660e85cf62
Salim <salim.hassan.welleat@gmail.com>
2025-09-16 02:39:17 +0200

    build: user respository

--- servers/server/src/modules/users/user.module.ts
+++ servers/server/src/modules/users/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserService } from './services/user.service';
+import { UserRepository } from './repository/user.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
   exports: [UserService],
 })
 export class UserModule {}
