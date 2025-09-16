2e6eaee41de63937f61662822c78dcb768c5b68d
Salim <salim.hassan.welleat@gmail.com>
2025-09-16 18:47:14 +0200

    built: signup

--- servers/server/src/modules/users/services/user.service.ts
+++ servers/server/src/modules/users/services/user.service.ts
@@ -3,18 +3,29 @@ import { User } from '../entity/user.entity';
 import { Repository } from 'typeorm';
 import { USER_REPOSITORY } from 'src/common/constants/constants';
 import { CreatedUserDto } from '../dto/users.dto';
+import { JwtTokenService } from 'src/modules/auth/services/jwt-token.service';
 
 @Injectable()
 export class UserService {
-  constructor(@Inject(USER_REPOSITORY) private userRepo: Repository<User>) {}
+  constructor(
+    private jwtTokenService: JwtTokenService,
+    @Inject(USER_REPOSITORY) private userRepo: Repository<User>,
+  ) {}
 
-  async createUser(data: Partial<User>): Promise<CreatedUserDto> {
+  async createUser(
+    data: Partial<User>,
+  ): Promise<CreatedUserDto & { token: string }> {
+    // Step 1: - Create user
     const user: User = this.userRepo.create(data); // prepares the entity
     const savedUser: User = await this.userRepo.save(user); // inserts into DB
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...rest } = savedUser; // exclude password field
-    return rest;
+
+    // Step 2:  - Prepare token
+    const token = this.jwtTokenService.createToken(savedUser.id);
+
+    return { ...rest, token };
   }
 
   async findAll(): Promise<User[]> {
