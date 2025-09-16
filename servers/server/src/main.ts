d1a236687e5e4432374f55beb01c6e23cb09ba31
Salim <salim.hassan.welleat@gmail.com>
2025-09-16 16:42:28 +0200

    integrated errors handler

--- servers/server/src/main.ts
+++ servers/server/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 import { morganConfig } from './config/morgan.config';
+import { HttpExceptionsErrorHandler } from './common/errors/http-exception-errors';
+import { processErrorHandler } from './common/errors/process-errors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Initialize Swagger
   setupSwagger(app);
+
+  // Errors handlers
+  // Register http exception errors handler
+  app.useGlobalFilters(new HttpExceptionsErrorHandler());
+  //  Register process errors handler
+  processErrorHandler(app);
 
   morganConfig(app);
 
