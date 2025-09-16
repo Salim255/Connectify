c5309c3af1366785b3883bf00330b14b18f217f7
Salim <doshka23@gmail.com>
2025-09-17 01:55:19 +0200

    built: profile creation response

--- servers/server/src/modules/profiles/controllers/profiles.controller.ts
+++ servers/server/src/modules/profiles/controllers/profiles.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ProfilesService } from '../services/profiles.service';
+import {
+  CreatedProfileResponseDto,
+  CreateProfileDto,
+} from '../dto/profiles.dto';
 
 @ApiTags('Profiles')
 @Controller('profiles')
@@ -9,9 +13,17 @@ export class ProfilesController {
 
   @Post()
   @ApiOperation({ summary: 'Create user profile' })
-  @ApiBody()
-  @ApiResponse()
-  async createProfile(){
-    
+  @ApiBody({
+    type: CreateProfileDto,
+    description: `Data to create a user's profile `,
+    required: true,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+    type: CreatedProfileResponseDto,
+  })
+  createProfile() {
+    return 'hello from create profile';
   }
 }
