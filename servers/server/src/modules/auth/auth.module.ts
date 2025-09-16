0fc0d78fdbd598cc53a5f4a9cfe950bb5019ccfb
Salim <doshka23@gmail.com>
2025-09-16 17:56:12 +0200

    built: jwt token service

--- servers/server/src/modules/auth/auth.module.ts
+++ servers/server/src/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { JwtTokenService } from './jws-token-service';
+import { JwtTokenService } from './services/jwt-token.service';
 
 @Module({
   imports: [
