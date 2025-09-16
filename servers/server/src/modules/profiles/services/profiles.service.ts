190b036e6bf969f11ca5be18552f43ebea70729b
Salim <doshka23@gmail.com>
2025-09-17 01:26:11 +0200

    built: profile service

--- servers/server/src/modules/profiles/services/profiles.service.ts
+++ servers/server/src/modules/profiles/services/profiles.service.ts
@@ -1,3 +1,11 @@
-export class  ProfilesService {
-    constructor(){}
+import { PROFILE_REPOSITORY } from '../../../common/constants/constants';
+import { Profile } from '../entity/profile.entity';
+import { Repository } from 'typeorm';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProfilesService {
+  constructor(
+    @Inject(PROFILE_REPOSITORY) private profileRep: Repository<Profile>,
+  ) {}
 }
