339d0d05947f59bfdcf9beac421eea12a67bf966
Salim <salim.hassan.welleat@gmail.com>
2025-09-18 01:42:33 +0200

    built: create chatUser router

--- servers/server/src/modules/chat-users/controllers/chat-users.controller.ts
+++ servers/server/src/modules/chat-users/controllers/chat-users.controller.ts
@@ -1,8 +1,27 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateChatUserDto } from '../dto/chat-users.dto';
+import { ChatUser } from '../entity/chatUser.entity';
 
 @ApiTags('ChatUsers')
 @Controller('chatUsers')
 export class ChatUsersController {
   constructor() {}
+  @Post()
+  @ApiOperation({
+    summary: 'Create chatUsers router',
+  })
+  @ApiBody({
+    description: 'Create chatUser payload',
+    type: CreateChatUserDto,
+    required: true,
+  })
+  @ApiResponse({
+    description: '',
+    type: ChatUser,
+    status: 201,
+  })
+  createChatUser() {
+    return 'hello from chatUser creation';
+  }
 }
