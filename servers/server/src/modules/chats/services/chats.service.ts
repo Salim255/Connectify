4d05991ae58a95a6848951ddd2a0677327fc538e
Salim <doshka23@gmail.com>
2025-09-18 01:36:55 +0200

    added: chatusers controller

--- servers/server/src/modules/chats/services/chats.service.ts
+++ servers/server/src/modules/chats/services/chats.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class ChatsService {
   constructor(@Inject(CHAT_REPOSITORY) private chatRepo: Repository<Chat>) {}
+
   async createChat(): Promise<Chat> {
     const chat: Chat = this.chatRepo.create();
     return this.chatRepo.save(chat);
