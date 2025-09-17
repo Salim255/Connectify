85eefa62fc44750b09dc2fa4ef3c0c8008adff78
Salim <doshka23@gmail.com>
2025-09-17 02:32:22 +0200

    built: create profile route

--- servers/server/src/modules/profiles/profiles.module.ts
+++ servers/server/src/modules/profiles/profiles.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { profileRepository } from './repository/profile.repository';
 import { ProfilesService } from './services/profiles.service';
+import { ProfilesController } from './controllers/profiles.controller';
 
 @Module({
   imports: [DatabaseModule],
   providers: [profileRepository, ProfilesService],
+  controllers: [ProfilesController],
 })
 export class ProfilesModule {}
