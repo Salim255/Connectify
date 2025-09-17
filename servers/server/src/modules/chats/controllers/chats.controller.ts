0d2ab84807d0b031d922d2ac68b35afcbdebab4f
Salim <salim.hassan.welleat@gmail.com>
2025-09-18 01:20:55 +0200

    built: chatdto

--- servers/server/src/modules/chats/controllers/chats.controller.ts
+++ servers/server/src/modules/chats/controllers/chats.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateChatDto, CreateChatResponseDto } from '../dto/chats-dto';
 
 @ApiTags('Chats')
 @Controller('chats')
 export class ChatsController {
   @Post()
-  @ApiOperation()
-  @ApiBody()
-  @ApiResponse()
+  @ApiOperation({ summary: 'Create chat router' })
+  @ApiBody({
+    type: CreateChatDto,
+    required: true,
+    description: 'Create chat payload',
+  })
+  @ApiResponse({
+    status: 201,
+    type: CreateChatResponseDto,
+    description: 'Create chat response',
+  })
   createChat() {
     return 'Hello from create chat';
-}
+  }
 }
