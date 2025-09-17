a69389d53e417a7bb405079ff7c6474619bbc0ad
Salim <salim.hassan.welleat@gmail.com>
2025-09-17 14:08:10 +0200

    built: match service

--- servers/server/src/modules/matches/services/matches.service.ts
+++ servers/server/src/modules/matches/services/matches.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { MATCH_REPOSITORY } from 'src/common/constants/constants';
+import { Repository } from 'typeorm';
+import { Match } from '../entity/match.entity';
 
 @Injectable()
-export class MatchesService {}
+export class MatchesService {
+  constructor(@Inject(MATCH_REPOSITORY) private matchRepo: Repository<Match>) {}
+  async initiateMatch() {}
+}
