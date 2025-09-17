fbfc7067daa287c35a633280895fbf5398ef7b36
Salim <doshka23@gmail.com>
2025-09-18 01:59:24 +0200

    built: create chat usr handler

--- servers/server/src/modules/chat-users/services/chat-users.service.ts
+++ servers/server/src/modules/chat-users/services/chat-users.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ChatUser } from '../entity/chat-user.entity';
+import { CHAT_USER_REPOSITORY } from 'src/common/constants/constants';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ChatUsersService {}
+export class ChatUsersService {
+  constructor(
+    @Inject(CHAT_USER_REPOSITORY) private chatUserRepo: Repository<ChatUser>,
+  ) {}
+  async createChatUser(): Promise<ChatUser> {
+    const chatUser: ChatUser = this.chatUserRepo.create();
+    return await this.chatUserRepo.save(chatUser);
+  }
+}
