32d68e71966e9dde752ec82b9a96f712bdf03623
Salim <doshka23@gmail.com>
2025-09-18 01:20:55 +0200

    built: chatdto

--- servers/server/src/modules/chats/dto/chats-dto.ts
+++ servers/server/src/modules/chats/dto/chats-dto.ts
@@ -1,3 +1,12 @@
-export class CreateChatDto {
-    
+import { ApiProperty } from '@nestjs/swagger';
+import { Chat } from '../entity/chat.entity';
+
+export class CreateChatDto {}
+
+export class CreateChatResponseDto {
+  @ApiProperty({ description: 'Creation chat status' })
+  status: string;
+  data: {
+    chat: Chat;
+  };
 }
