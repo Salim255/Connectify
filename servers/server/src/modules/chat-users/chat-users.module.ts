f07d9aaedad6e57e164b9aba045a8e4f4c95f480
Salim <salim.hassan.welleat@gmail.com>
2025-09-18 01:45:05 +0200

    built: chatuser module

---
+++ servers/server/src/modules/chat-users/chat-users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../database/database.module';
+import { ChatUsersController } from './controllers/chat-users.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  providers: [],
+  controllers: [ChatUsersController],
+})
+export class ChatUsersModule {},
