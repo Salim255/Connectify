037ace8ffde4c49230456adcdbf7c2ad8afe4448
Salim <salim.hassan.welleat@gmail.com>
2025-09-18 02:52:57 +0200

    added:  MESSAGE_REPOSITORY constant

--- servers/server/src/modules/messages/controllers/messages.controller.ts
+++ servers/server/src/modules/messages/controllers/messages.controller.ts
@@ -1,14 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Messages')
 @Controller('messages')
 export class MessagesController {
-    @Post()
-    @ApiOperation()
-    @ApiBody()
-    @ApiResponse()
-    createMessage(){
-        return 'Hello from create message'
+  @Post()
+  @ApiOperation({
+    description: 'Create message router',
+  })
+  @ApiBody({
+    type: CreateMessageDto,
+    description: 'Create message payload',
+    required: true,
+  })
+  @ApiResponse({
+    type: Message,
+    description: 'Created message',
+    status: 201,
+  })
+  createMessage(): Promise<Message>{
+      return 'Hello from create message'
     }
 }
