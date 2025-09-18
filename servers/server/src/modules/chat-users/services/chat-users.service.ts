f4a8166e3555fe5271ddb22ee63ef1b0d4f66aad
Salim <doshka23@gmail.com>
2025-09-18 02:39:05 +0200

    all is good for create chat user

--- servers/server/src/modules/chat-users/services/chat-users.service.ts
+++ servers/server/src/modules/chat-users/services/chat-users.service.ts
@@ -2,14 +2,15 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ChatUser } from '../entity/chat-user.entity';
 import { CHAT_USER_REPOSITORY } from 'src/common/constants/constants';
 import { Repository } from 'typeorm';
+import { CreateChatUserDto } from '../dto/chat-users.dto';
 
 @Injectable()
 export class ChatUsersService {
   constructor(
     @Inject(CHAT_USER_REPOSITORY) private chatUserRepo: Repository<ChatUser>,
   ) {}
-  async createChatUser(): Promise<ChatUser> {
-    const chatUser: ChatUser = this.chatUserRepo.create();
+  async createChatUser(data: CreateChatUserDto): Promise<ChatUser> {
+    const chatUser: ChatUser = this.chatUserRepo.create(data);
     return await this.chatUserRepo.save(chatUser);
   }
 }
