226c95552de20668fa72da46fa298b4e35080e40
Salim <salim.hassan.welleat@gmail.com>
2025-09-18 16:15:58 +0200

    built: profile routes protection

--- servers/server/src/modules/profiles/profiles.module.ts
+++ servers/server/src/modules/profiles/profiles.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from '../../database/database.module';
 import { profileRepository } from './repository/profile.repository';
 import { ProfilesService } from './services/profiles.service';
 import { ProfilesController } from './controllers/profiles.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   providers: [profileRepository, ProfilesService],
   controllers: [ProfilesController],
 })
