48ec16b273f8891f70d27b6cf5cc9e217cff330b
Salim <doshka23@gmail.com>
2025-09-18 13:38:36 +0200

    built: get all chats route

--- servers/server/src/modules/chats/dto/chats-dto.ts
+++ servers/server/src/modules/chats/dto/chats-dto.ts
@@ -6,7 +6,19 @@ export class CreateChatDto {}
 export class CreateChatResponseDto {
   @ApiProperty({ description: 'Creation chat status' })
   status: string;
+
+  @ApiProperty({ description: 'Response data' })
   data: {
     chat: Chat;
+  };
+}
+
+export class GetAllChatsResponseDto {
+  @ApiProperty({ description: 'Get all chats status' })
+  status: string;
+
+  @ApiProperty({ description: 'Response data' })
+  data: {
+    chats: Chat[];
   };
 }
