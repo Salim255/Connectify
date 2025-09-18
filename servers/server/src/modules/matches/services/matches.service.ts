9a235112b442e0e60372665705b2d2038abc7654
Salim <salim.hassan.welleat@gmail.com>
2025-09-18 19:34:42 +0200

    built: update match service

--- servers/server/src/modules/matches/services/matches.service.ts
+++ servers/server/src/modules/matches/services/matches.service.ts
@@ -1,7 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { MATCH_REPOSITORY } from 'src/common/constants/constants';
 import { Repository } from 'typeorm';
-import { Match } from '../entity/match.entity';
+import { Match, MatchStatus } from '../entity/match.entity';
 import { CreateMatchDto } from '../dto/matches-dto';
 
 @Injectable()
@@ -15,5 +15,15 @@ export class MatchesService {
 
   async getAllMatches(): Promise<Match[]> {
     return await this.matchRepo.find();
+  }
+
+  async acceptMatch(matchId: string): Promise<Match> {
+    await this.matchRepo.update(matchId, { status: MatchStatus.MATCHED });
+
+    const updatedMatch = await this.matchRepo.findOneBy({ id: matchId });
+    if (!updatedMatch) {
+      throw new NotFoundException(`Match with ID ${matchId} not found`);
+    }
+    return this.matchRepo.save(updatedMatch);
   }
 }
