3e87118d13cd17559380b7c00f68dcef55ab2ead
Salim <salim.hassan.welleat@gmail.com>
2025-09-19 00:44:22 +0200

    some update in match model on client side

--- client/src/app/features/matches/model/match.model.ts
+++ client/src/app/features/matches/model/match.model.ts
@@ -1,10 +1,20 @@
 
 import { Profile } from "../../profile/model/profile.model";
 
+export enum MatchStatus {
+  PENDING = 'pending', // maybe one-sided like
+  MATCHED = 'matched', // mutual like
+  BLOCKED = 'blocked', // one user blocked the other
+  UNMATCHED = 'unmatched', // match ended
+}
+
 export interface Match {
-  id: string;                          // match ID
-  profile: Profile;                    // matched profile
-  matchedAt: Date;                     // timestamp of the match
-  isMutual: boolean;                   // true if both liked
-  seen?: boolean;                      // if user saw this match
+  id: string;           // match ID
+  profile: Profile;     // matched profile
+  matchedAt?: Date;     // timestamp of the match
+  status: MatchStatus;  // true if both liked
+  isFavorite: boolean;
+  isHidden: boolean,
+  createdAt: Date;
+  updatedAt: Date;
 }
