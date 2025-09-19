fe0c603ed2a1d317588429ddb874eb6f8069634e
Salim <doshka23@gmail.com>
2025-09-19 19:19:36 +0200

    updated: the client env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   GoogleMapAPIKey: 'AIzaSyDXBYmaAbWu09gdPod6qNYuQSpZx7kGA44',
   mapBaseUrl: 'https://api.opencagedata.com/geocode/v1/json',
+  apiUrl: 'http://localhost:3000/api/v1',
 };
