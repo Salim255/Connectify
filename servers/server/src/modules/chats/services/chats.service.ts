6b4a9134216464c7a0f3b07182c953905cfc8242
Salim <doshka23@gmail.com>
2025-09-19 13:29:27 +0200

    built: create chat with message and chatuser service

--- servers/server/src/modules/chats/services/chats.service.ts
+++ servers/server/src/modules/chats/services/chats.service.ts
@@ -1,11 +1,61 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CHAT_REPOSITORY } from 'src/common/constants/constants';
 import { Chat } from '../entity/chat.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import {
+  ChatUser,
+  ChatUserRole,
+} from 'src/modules/chat-users/entity/chat-user.entity';
+import { Message } from 'src/modules/messages/entity/message.entity';
 
 @Injectable()
 export class ChatsService {
-  constructor(@Inject(CHAT_REPOSITORY) private chatRepo: Repository<Chat>) {}
+  constructor(
+    private dataSource: DataSource,
+    @Inject(CHAT_REPOSITORY) private chatRepo: Repository<Chat>,
+  ) {}
+
+  async createChatWithMessage(
+    currentUserId: string,
+    recipientId: string,
+    messageText: string,
+  ) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      // 1. Create chat
+      const chat = this.chatRepo.create();
+      const createdChat: Chat = await queryRunner.manager.save(chat);
+
+      // 2. Add participants
+      await queryRunner.manager.save(ChatUser, [
+        {
+          chatId: createdChat.id,
+          profileId: currentUserId,
+          roles: [ChatUserRole.ADMIN],
+        },
+        { chatId: createdChat.id, profileId: recipientId },
+      ]);
+
+      // 3. Send first message
+      await queryRunner.manager.save(Message, {
+        chatId: chat.id,
+        senderId: currentUserId,
+        content: messageText,
+        sentAt: new Date(),
+      });
+
+      await queryRunner.commitTransaction();
+      return chat;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
+  }
 
   async createChat(): Promise<Chat> {
     const chat: Chat = this.chatRepo.create();
