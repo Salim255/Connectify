54ab7f7b1b4c3f2ee5c4c03f0980f41222067fe3
Salim <salim.hassan.welleat@gmail.com>
2025-09-20 13:41:52 +0200

    added:  ProfilesResponse type

--- client/src/app/features/browse/services/browse-http.service.ts
+++ client/src/app/features/browse/services/browse-http.service.ts
@@ -2,7 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Profile } from "../../profile/model/profile.model";
 
+export type ProfilesResponse = {
+  status: 'Success';
+  data: {
+    profiles: Profile []
+  }
+}
 @Injectable({providedIn: 'root'})
 export class BrowseHttpService {
   private ENV = environment;
