4fc26f3fd9e0a42cd7a7317d63ae218f19772675
Salim <doshka23@gmail.com>
2025-09-20 14:07:35 +0200

    built: fetchMatches method

--- client/src/app/features/matches/services/matches-http.service.ts
+++ client/src/app/features/matches/services/matches-http.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable({providedIn: 'root'})
 
 export class MatchesHttpService {
   private ENV = environment;
+  private baseUrl: string = `${this.ENV.apiUrl}/matches`;
 
+  constructor(private http: HttpClient){}
+
+  fetchMatches():Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}/users`);
+  }
 }
