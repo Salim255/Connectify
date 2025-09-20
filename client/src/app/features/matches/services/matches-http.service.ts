5f55b7bd3396278e7a1e42e377695ca811a8b3a8
Salim <doshka23@gmail.com>
2025-09-20 14:31:07 +0200

    added: MatchesResponse

--- client/src/app/features/matches/services/matches-http.service.ts
+++ client/src/app/features/matches/services/matches-http.service.ts
@@ -2,9 +2,16 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Match } from "../model/match.model";
+
+export type MatchesResponse =  {
+  status: 'Success';
+  data: {
+    matches: Match[]
+  }
+}
 
 @Injectable({providedIn: 'root'})
-
 export class MatchesHttpService {
   private ENV = environment;
   private baseUrl: string = `${this.ENV.apiUrl}/matches`;
