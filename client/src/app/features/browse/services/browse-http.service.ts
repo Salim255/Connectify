41a0563e49ece7df8d7c930f003c8d5e29cbe0db
Salim <doshka23@gmail.com>
2025-09-20 13:24:41 +0200

    built: browse http service baseUlr

--- client/src/app/features/browse/services/browse-http.service.ts
+++ client/src/app/features/browse/services/browse-http.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class BrowseHttpService {
-
+    private ENV = environment;
+    private baseUrl: string = ``;
 }
