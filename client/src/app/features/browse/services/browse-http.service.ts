99407d56ebfd4c737d582aa31e40caa0db5d6207
Salim <salim.hassan.welleat@gmail.com>
2025-09-20 13:27:18 +0200

    built: fetch profiles for browse component

--- client/src/app/features/browse/services/browse-http.service.ts
+++ client/src/app/features/browse/services/browse-http.service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class BrowseHttpService {
-    private ENV = environment;
-    private baseUrl: string = ``;
+  private ENV = environment;
+  private baseUrl: string = `${this.ENV.apiUrl}/profiles`;
+
+  constructor(private http: HttpClient){}
+
+  fetchProfiles():Observable<any>{
+    return this.http.get<any>(this.baseUrl);
+  }
 }
