9ad45b6331973f0cec717f8f7fb3b9f7ff7a4329
Salim <salim.hassan.welleat@gmail.com>
2025-09-20 14:43:19 +0200

    built: http fetchChats

--- client/src/app/features/chats/services/chats-http.service.ts
+++ client/src/app/features/chats/services/chats-http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable({providedIn: 'root'})
@@ -8,4 +9,8 @@ export class ChatsHttpService {
   private baseUrl: string = `${this.ENV.apiUrl}/chats`;
 
   constructor(private http: HttpClient){}
+
+  fetchChats(): Observable<any>{
+    return this.http.get<any>(this.baseUrl);
+  }
 }
