300f6db8331962d950f9e7f13d0f95a7ca1610a1
Salim <doshka23@gmail.com>
2025-09-20 14:19:20 +0200

    built: matches component ngOnDestroy

--- client/src/app/features/matches/matches.component.ts
+++ client/src/app/features/matches/matches.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MatchesService } from "./services/matches.service";
 import { Match } from "./model/match.model";
 import { PAGES } from "src/app/shared/components/header/header.component";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-matches',
@@ -10,11 +11,23 @@ import { PAGES } from "src/app/shared/components/header/header.component";
   standalone: false
 })
 
-export class MatchesComponent {
+export class MatchesComponent implements OnInit {
   pageName: PAGES = PAGES.MATCHES;
   matches: Match [];
 
+  matchesSubscription!: Subscription;
+
   constructor(private matchesService: MatchesService){
     this.matches = this.matchesService.MATCHES_PLACEHOLDER;
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+
+  ngOnDestroy(): void {
+    this.matchesSubscription?.unsubscribe();
   }
 }
