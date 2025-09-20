9a7f6b1082cec1d4eb4eeb8aafb0bc119da2b58c
Salim <doshka23@gmail.com>
2025-09-20 13:29:33 +0200

    built: getProfiles method

--- client/src/app/features/browse/services/browse.serve.ts
+++ client/src/app/features/browse/services/browse.serve.ts
@@ -1,11 +1,25 @@
 import { Injectable } from "@angular/core";
 import { Profile } from "../../profile/model/profile.model";
 import { ProfileService } from "../../profile/services/profile.service";
+import { BrowseHttpService } from "./browse-http.service";
+import { Observable, tap } from "rxjs";
 @Injectable({providedIn: 'root'})
 
 export class BrowseService {
   BROWSE_PROFILES: Profile [];
-  constructor(private profileService: ProfileService){
+
+  constructor(
+    private browseHttpService: BrowseHttpService,
+    private profileService: ProfileService,
+  ){
     this.BROWSE_PROFILES  = this.profileService.PROFILES_PLACEHOLDER;
+  }
+
+  getProfiles():Observable<any>{
+    return this.browseHttpService.fetchProfiles().pipe(
+      tap((response) => {
+        console.log(response);
+      })
+    );
   }
 }
