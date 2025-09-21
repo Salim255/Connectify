b7deaf9786853190d60725976486c74058ab7a10
Salim <doshka23@gmail.com>
2025-09-21 21:39:06 +0200

    built: ChatResponse

--- client/src/app/features/chat/services/chat-http.service.ts
+++ client/src/app/features/chat/services/chat-http.service.ts
@@ -2,6 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Chat } from "../model/chat.model";
+
+export type ChatResponse = {
+  status: 'Success';
+  data: {
+    chat: Chat
+  }
+}
 
 @Injectable({providedIn: 'root'})
 export class ChatHttpService {
