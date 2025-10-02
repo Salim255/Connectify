9264db4e6072ad989475ca51ceac9fddaba6a711
Salim <doshka23@gmail.com>
2025-10-02 12:30:51 +0200

    built: ws guard

--- servers/server/src/modules/auth/guard/jwt-token-ws.guard.ts
+++ servers/server/src/modules/auth/guard/jwt-token-ws.guard.ts
@@ -1,8 +1,28 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { JwtTokenService } from '../services/jwt-token.service';
+
+interface AuthenticatedSocket extends Socket {
+  data: {
+    userId: string;
+    // Add other fields if needed
+  };
+}
 
 @Injectable()
 export class JwtWsAuthGuard implements CanActivate {
+  constructor(private jwtTokenService: JwtTokenService) {}
   canActivate(context: ExecutionContext): boolean {
-    return true;
+    const client: AuthenticatedSocket = context.switchToWs().getClient();
+    const { token } = client.handshake.auth as { token?: string };
+
+    if (!token) return false;
+    try {
+      const { id: userId } = this.jwtTokenService.verifyToken(token);
+      client.data.userId = userId;
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
