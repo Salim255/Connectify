7206b253db01866769bace1e88f244f4c622cd35
Salim <doshka23@gmail.com>
2025-10-02 11:07:34 +0200

    build: injected core socket on chat gatway

--- client/src/app/features/chat/gateway/chat.gateway.ts
+++ client/src/app/features/chat/gateway/chat.gateway.ts
@@ -1,9 +1,26 @@
-import { Injectable, OnInit } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Socket } from "socket.io-client";
+import { SocketCoreService } from "src/app/socket/services/socket-presence.service";
 
 @Injectable({providedIn: 'root'})
 
 export class ChatGatewayService {
-  constructor(){}
+  private socket: Socket;
+
+  constructor(private socketCore: SocketCoreService){
+    const socketInstance = this.socketCore.getSocket();
+
+    if (!socketInstance) {
+      throw new Error('SocketCoreService must be initialized before using ChatGatewayService');
+    }
+
+    this.socket = socketInstance;
+    this.registerListeners();
+  }
+
+  private registerListeners(): void {
+
+  }
 
   initializeChatGateway(){
     console.log('Hello from Chat gateway')
